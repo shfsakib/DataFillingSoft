826edb5313f3354c6a3b4b6934c5cf557b220a70
Anonymous <40665494+shfsakib@users.noreply.github.com>
2021-10-27 18:49:17 +0600

    login design till textbox

---
+++ DataFillingSoftDeskApp/DataFillingSoftDeskApp/ui/log-in.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace DataFillingSoftDeskApp.ui
+{
+    partial class log_in
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnloginClose = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblDevelop = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.panelUserName = new System.Windows.Forms.Panel();
+            this.panelPass = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkCyan;
+            this.panel1.Controls.Add(this.lblDevelop);
+            this.panel1.Controls.Add(this.lblName);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(303, 518);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnloginClose
+            // 
+            this.btnloginClose.BackgroundImage = global::DataFillingSoftDeskApp.Properties.Resources.close;
+            this.btnloginClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnloginClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnloginClose.FlatAppearance.BorderSize = 0;
+            this.btnloginClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnloginClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnloginClose.Location = new System.Drawing.Point(724, 12);
+            this.btnloginClose.Name = "btnloginClose";
+            this.btnloginClose.Size = new System.Drawing.Size(31, 26);
+            this.btnloginClose.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.btnloginClose, "Close");
+            this.btnloginClose.UseVisualStyleBackColor = true;
+            this.btnloginClose.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DataFillingSoftDeskApp.Properties.Resources.laptop;
+            this.pictureBox1.Location = new System.Drawing.Point(99, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblName.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.White;
+            this.lblName.Location = new System.Drawing.Point(12, 172);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(212, 38);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Company Name";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDevelop
+            // 
+            this.lblDevelop.AutoSize = true;
+            this.lblDevelop.ForeColor = System.Drawing.Color.White;
+            this.lblDevelop.Location = new System.Drawing.Point(139, 494);
+            this.lblDevelop.Name = "lblDevelop";
+            this.lblDevelop.Size = new System.Drawing.Size(152, 15);
+            this.lblDevelop.TabIndex = 2;
+            this.lblDevelop.Text = "Developed By Sakib Hossain";
+            this.lblDevelop.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(412, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Login to your account";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUserName.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserName.Location = new System.Drawing.Point(402, 220);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(5);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(294, 27);
+            this.txtUserName.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(402, 271);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(10);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(294, 27);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // panelUserName
+            // 
+            this.panelUserName.BackColor = System.Drawing.Color.White;
+            this.panelUserName.Location = new System.Drawing.Point(360, 215);
+            this.panelUserName.Name = "panelUserName";
+            this.panelUserName.Size = new System.Drawing.Size(336, 38);
+            this.panelUserName.TabIndex = 5;
+            // 
+            // panelPass
+            // 
+            this.panelPass.BackColor = System.Drawing.Color.White;
+            this.panelPass.Location = new System.Drawing.Point(360, 266);
+            this.panelPass.Name = "panelPass";
+            this.panelPass.Size = new System.Drawing.Size(336, 38);
+            this.panelPass.TabIndex = 6;
+            // 
+            // log_in
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 518);
+            this.Controls.Add(this.panelPass);
+            this.Controls.Add(this.panelUserName);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnloginClose);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "log_in";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "log_in";
+            this.Load += new System.EventHandler(this.log_in_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.log_in_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.log_in_MouseMove);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnloginClose;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblDevelop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Panel panelUserName;
+        private System.Windows.Forms.Panel panelPass;
+    }
+}
---
+++ DataFillingSoftDeskApp/DataFillingSoftDeskApp/ui/log-in.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataFillingSoftDeskApp.ui
+{
+    public partial class log_in : Form
+    {
+        private Point mouse_offset;
+        public log_in()
+        {
+            InitializeComponent();
+            //place this code in your form constructor
+            btnloginClose.FlatAppearance.MouseOverBackColor = btnloginClose.BackColor;
+            btnloginClose.BackColorChanged += (s, e) => {
+                btnloginClose.FlatAppearance.MouseOverBackColor = btnloginClose.BackColor; 
+            };
+        }
+        private void log_in_Load(object sender, EventArgs e)
+        {
+            lblName.Text = String.Format("Universal Education{0} And Research", "\r\n"); 
+            panelUserName.SendToBack();
+            panelPass.SendToBack();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void log_in_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouse_offset = new Point(-e.X, -e.Y);
+        }
+
+        private void log_in_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                Point mousePos = Control.MousePosition;
+                mousePos.Offset(mouse_offset.X, mouse_offset.Y);
+                this.Location = mousePos; //move the form to the desired location
+            }
+        }
+
+       
+
+        private void txtUserName_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.Text = null;
+        }
+    }
+}
